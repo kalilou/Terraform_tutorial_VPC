ef525b2d19990c2116af224319e09435266cfae4
Kalilou Diaby <kalilou.diaby@blocket.se>
2016-05-19 11:09:30 +0200

    Change the vpc cidr

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 
 # Create a VPC to launch our instances into
 resource "aws_vpc" "vpc_tuto" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "172.31.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
@@ -52,7 +52,7 @@ resource "aws_route" "private_route" {
 # Create a subnet in the AZ eu-west-1a
 resource "aws_subnet" "subnet_eu_west_1a" {
   vpc_id                  = "${aws_vpc.vpc_tuto.id}"
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "172.31.1.0/24"
   map_public_ip_on_launch = true
   availability_zone = "eu-west-1a"
   tags = {
@@ -63,7 +63,7 @@ resource "aws_subnet" "subnet_eu_west_1a" {
 # Create a subnet in the AZ eu-west-1b
 resource "aws_subnet" "subnet_eu_west_1b" {
   vpc_id                  = "${aws_vpc.vpc_tuto.id}"
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "172.31.2.0/24"
   availability_zone = "eu-west-1b"
   tags = {
   	Name =  "Subnet az 1b"
@@ -73,7 +73,7 @@ resource "aws_subnet" "subnet_eu_west_1b" {
 # Create a subnet in the AZ eu-west-1c
 resource "aws_subnet" "subnet_eu_west_1c" {
   vpc_id                  = "${aws_vpc.vpc_tuto.id}"
-  cidr_block              = "10.0.3.0/24"
+  cidr_block              = "172.31.3.0/24"
   availability_zone = "eu-west-1c"
   tags = {
   	Name =  "Subnet az 1c"
@@ -111,6 +111,3 @@ resource "aws_route_table_association" "subnet_eu_west_1c_association" {
     subnet_id = "${aws_subnet.subnet_eu_west_1c.id}"
     route_table_id = "${aws_route_table.private_route_table.id}"
 }
-
-
-
